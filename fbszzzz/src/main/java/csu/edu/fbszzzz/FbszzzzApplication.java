fe21928efb83b90cca52ee8e6962a70163cda8d6
fzs <2518816287>
2026-04-11 11:39:33 +0800

    新建项目

---
+++ fbszzzz/src/main/java/csu/edu/fbszzzz/FbszzzzApplication.java
@@ -0,0 +1,13 @@
+package csu.edu.fbszzzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbszzzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbszzzzApplication.class, args);
+	}
+
+}
---
+++ fbszzzz/src/test/java/csu/edu/fbszzzz/FbszzzzApplicationTests.java
@@ -0,0 +1,13 @@
+package csu.edu.fbszzzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbszzzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
